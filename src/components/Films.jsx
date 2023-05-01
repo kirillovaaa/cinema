2844ced4033dfdd4027bea9d9d942bbe01b85168
Aleksandra Kirillova <kiri2lova@gmail.com>
2023-05-01 12:55:46 +0300

    feat: сделать часть films

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -1,6 +1,27 @@
 import React from "react";
+import supernatural from "../images/supernatural.jpg";
 import "./Films.css";
 
-const Films = () => <div className="films"></div>;
+const Films = () => (
+  <div className="films">
+    <div class="films-header">
+      <h2 className="films-title">Самый популярный фильм на неделе</h2>
+      <button className="films-search-button" type="button">
+        Посмотреть остальные фильмы
+      </button>
+    </div>
+
+    <div className="films-background">
+      <img
+        className="films-supernatural"
+        src={supernatural}
+        alt="Сверхъестественное"
+      ></img>
+      <button className="films-watch" type="button">
+        Посмотреть
+      </button>
+    </div>
+  </div>
+);
 
 export default Films;
